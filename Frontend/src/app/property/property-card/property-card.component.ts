c1baa1c8bfa36515917be3a52504febf685844dc
Shah <dshah@proforma.com>
2020-07-01 16:57:37 -0400

    FIrst Hose added

---
+++ Frontend/src/app/property/property-card/property-card.component.ts
@@ -0,0 +1,24 @@
+import  {Component } from '@angular/core';
+
+
+
+
+@Component({
+  selector:'app-property-card',
+  //template:'<h1> Hi There  This is a Card</h1>',
+  templateUrl: 'property-card.component.html',
+  styleUrls: ['property-card.component.css']
+
+
+
+})
+export class PropertyCardComponent{
+
+  Property: any ={
+    "Id": 1,
+    "Name":"Birla House",
+    "Type":"House",
+    "Price":179000
+  }
+
+}
---
+++ Frontend/src/app/property/property-list/property-list.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-property-list',
+   //template: '<h1> Hi There  This is a SubList</h1>'
+  templateUrl: './property-list.component.html',
+  //styleUrls: ['./property-list.component.css']
+})
+export class PropertyListComponent  {
+
+
+
+}
